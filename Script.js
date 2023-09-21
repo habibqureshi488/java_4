e4ac1a30ec519e63e0b221ef664701ba0d405565
Habib <129363789+habibqureshi488@users.noreply.github.com>
2023-09-21 12:59:38 +0500

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,742 @@
+
+// switch (userInput) {
+//   case 1:
+//   case 2:
+//   case 4:
+//   case 5:
+//   case 6:
+//   case 8:
+//   case 9:
+//     console.log("Better Luck Next Time");
+//     break;
+//   case 3:
+//     console.log("Congratulations! You Win a Brand New Honda Bike");
+//     break;
+//   case 7:
+//     console.log("Congratulations! You Win a Honda Civic Rs Turbo");
+//     break;
+//   case 10:
+//     console.log("Congratulations! You Win a Sogo ka Generator Iron Juicer Blender");
+//     break;
+//   default:
+//     console.log("Invalid input. Please enter a number between 1 and 10.");
+//     break;
+// }
+
+
+
+// let randomNumber = Math.floor(Math.random() * 10) + 1;
+
+
+// switch (randomNumber) {
+//   case 1:
+//   case 2:
+//   case 4:
+//   case 5:
+//   case 6:
+//   case 8:
+//   case 9:
+//     console.log("Better Luck Next Time");
+//     break;
+//   case 3:
+//     console.log("Congratulations! You Win a Brand New Honda Bike");
+//     break;
+//   case 7:
+//     console.log("Congratulations! You Win a Honda Civic Rs Turbo");
+//     break;
+//   case 10:
+//     console.log("Congratulations! You Win a Sogo ka Generator Iron Juicer Blender");
+//     break;
+//   default:
+//     console.log("I cannot determine the current time.");
+//     break;
+// }
+
+
+// ********************* _____ ----------!!!`~`(((Chapter No 3)))`~`!!!---------- _____  *********************
+
+// Advance Data type 
+// 1. Object
+
+// let student_Father_Name = "";
+// let student_Class = "";
+// let student_Address = "";
+// let student_Email = "";
+// let student_City = "";
+// let student_Name = "";
+// let student_School_Name = "";
+// let student_PhoneNo = "";
+
+// Arrays Practice at Home.......!
+
+
+
+
+// let a = ["Milk" , "Bread" , "Apples"]
+// let size = a.length;
+// a[1]="Banana";
+// console.log(a)
+
+// let a = ["Milk" , "Bread" , "Apples"]
+// a.splice(2,0,"orange");
+// console.log(a)
+
+// let a = ["Milk" , "Bread" , "Apples1" , "Apples"]
+// a.splice(2,1,"orange");
+// console.log(a)
+
+// let a = ["Milk" , "Bread" , "Guava" , "Apples"]
+// let size  = a.lenght;
+// a.splice(2,0,"orange");
+// a.splice(3,0,"Lehcee");
+// a.splice(4,0,"Mango" ,"Kiwi" , "Peanut butter");
+// console.log(a)
+
+
+// array Mreginn
+
+// let a = ["Milk" , "Bread" , "Oats"];
+// let b = ["Orange" , "Bananas" , "Apples"];
+// let c = a.concat(b);
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// delete LAst Item in ARRay
+
+// let a = ["Milk" , "Bread" , "Oats"];
+// let b = ["Orange" , "Bananas" , "Apples"];
+// let c = a.concat(b);
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// c.pop();
+// console.log(c);
+// last console is important
+
+// delete frist Item in ARRay
+
+// let a = ["Milk" , "Bread" , "Oats"];
+// let b = ["Orange" , "Bananas" , "Apples"];
+// let c = a.concat(b);
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// c.shift();
+// console.log(c);
+
+// add frist item in array 
+
+// let a = ["Milk" , "Bread" , "Oats"];
+// let b = ["Orange" , "Bananas" , "Apples"];
+// let c = a.concat(b);
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// c.unshift("Juice");
+// console.log(c);
+
+
+// line wise name calll in array (shorting Data)
+
+// let a = ["Milk" , "Bread" , "Oats"];
+// let b = ["Orange" , "Bananas" , "Apples"];
+// let c = a.concat(b);
+// console.log(c)
+// c.sort();
+// console.log(c)
+
+// with number (shorting data)\
+
+// let num =  [1,100,32,35,64,76,54,45,43,65,87,875,54,757,36,46,78,89]
+// console.log(num);
+// num.sort();
+// console.log(num);
+
+// line wise name calll in array (shorting Data) re-vers
+
+
+// let a = ["Milk" , "Bread" , "Oats"];
+// let b = ["Orange" , "Bananas" , "Apples"];
+// let c = a.concat(b);
+// console.log(c)
+// c.reverse();
+// console.log(c)
+
+
+// practice
+
+// let empty = []
+// empty[0] = "Milk" 
+// empty[1] = "Bread" 
+// empty[2] = "Apple"
+// empty.splice(1,0,"Banana","Eggs");
+// empty.pop();
+// // empty.short();
+// empty.splice(2,0,"Carrot","Lettuce");
+// let em = ["juices", "pop"]
+// let final = empty.concat(em);
+// final.pop();
+
+// console.log(empty)
+// console.log(em)
+// final.sort();
+// console.log(final)
+
+
+// Index
+
+// let num =  ["Orange" , "Bananas" , "Apples"]
+// let index = num.indexOf(35)
+
+// console.log(num)
+// console.log(index)
+
+// // Index last
+
+// let num1 =  ["Orange" , "Bananas" , "Apples"]
+// let index1 = num1.lastIndexOfindexOf(35)
+
+// console.log(num1)
+// console.log(index1)
+
+
+// let company = {
+//     companyName: "Healthy Candy",
+//     activity: "food manufacturing",
+//     address: {
+//        street: "2nd street",
+//        number: "123",
+//        zipcode: "33116",
+//        city: "Miami",
+//        state: "Florida"
+//     },
+//     yearOfEstablishment: 2021
+//  };
+//  console.log(company.address.number)
+
+
+// let company = {
+//     companyName: "Healthy Candy",
+//     activity: ["food manufacturing","food manufacturing","food manufacturing"],
+//     address: {
+//        street: "2nd street",
+//        number: "123",
+//        zipcode: "33116",
+//        city: "Miami",
+//        state: "Florida"
+//     },
+//     yearOfEstablishment: 2021
+//  };
+//  console.log(company.address.number)
+
+
+// let company = { companyName: "Healthy Candy",
+// activities: [ "food manufacturing",
+// "improving kids' health",
+// "manufacturing toys"],
+// address: [{
+// street: "2nd street",
+// number: "123",
+// zipcode: "33116",
+// city: "Miami",
+// state: "Florida"
+// },
+// {
+// street: "1st West avenue",
+// number: "5",
+// zipcode: "75001",
+// city: "Addison",
+// state: "Texas"
+// }],
+// yearOfEstablishment: 2021
+// };
+
+// console.log(company.address[0].city);
+// console.log(company.address[1].zipcode);
+// console.log(company.address[1].city);
+
+
+// End Chapter 3
+
+
+// let people = {
+//     friends: []
+//   };
+  
+  
+//   let  friend1 = {
+//     firstName: "Murtaza",
+//     lastName: "Hussain",
+//     id: 1
+//   };
+  
+//   let friend2 = {
+//     firstName: "Umer",
+//     lastName: "Farroq",
+//     id: 2
+//   };
+  
+//  let friend3 = {
+//     firstName: "M",
+//     lastName: "ALi",
+//     id: 3
+//   };
+  
+ 
+//   people.friends.push(friend1);
+//   people.friends.push(friend2);
+//   people.friends.push(friend3);
+  
+//   console.log(people.friends[1]);
+//   console.log(people.friends[0]);
+//   console.log(people.friends[2]);
+//   console.log(people);
+  
+// const myArr1 = [1,3,5,6,8,9,15];
+// console.log(myArr1.indexOf(0));
+// console.log(myArr1.indexOf(3));
+
+
+// const myArr2 = [];
+// myArr2[10] = 'test'
+// console.log(myArr2);
+// console.log(myArr2[2]);
+
+// const myArr3 = [3,6,8,9,3,55,553,434];
+// myArr3.sort();
+// myArr3.length = 0;
+// console.log(myArr3[0]);
+
+// Assiment 
+
+
+// let myArray = ["John", "Doe", "Smith"];
+
+
+// myArray.pop(); 
+// myArray.shift(); 
+
+
+// myArray.unshift("FIRST");
+
+
+// myArray[2] = "MIDDLE";
+
+
+// myArray[3] = "hello World";
+
+
+// myArray.push("LAST");
+
+
+// console.log(myArray);
+
+
+// Create an array to hold the inventory of store items
+// const theList = [];
+
+
+// const item1 = {
+//   name: 'Item 1',
+//   model: 'Model A',
+//   cost: 50,
+//   quantity: 10
+// };
+
+// const item2 = {
+//   name: 'Item 2',
+//   model: 'Model B',
+//   cost: 75,
+//   quantity: 5
+// };
+
+// const item3 = {
+//   name: 'Item 3',
+//   model: 'Model C',
+//   cost: 100,
+//   quantity: 3
+// };
+
+
+// theList.push(item1, item2, item3);
+// console.log(theList);
+// console.log("Quantity of the third item:", theList[2].quantity);
+// theList.push('Additional Data', { category: 'Electronics' });
+// console.log("Additional data:", theList[3]);
+// console.log("Object within the data structure:", theList[4]);
+
+
+
+// Loops
+
+// Reapeat All Things
+// Smll Example
+// let i =0;
+// for(i ; i < 10 ; i++){
+//     console.log("Hello World");
+// }
+
+// Increment
+// let a = 0;
+
+// for(a;a < 50 ;a++)
+// {
+//     console.log("Hello ` HABIB");
+// }
+
+// decrement
+
+// let a = 100;
+
+// for(a;a >0 ;a--)
+// {
+//     console.log("Hello ` HABIB");
+// }
+
+
+// let arr = [76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87,76,76,90,98,87,88,97,67,66,87]
+// console.log(arr.length)
+
+// let num = 0;
+
+// for(num; num < 279 ; num++)
+// {
+//     if( arr[num] > 80)
+//     {
+//        console.log(`Student ${num} You are passed`)
+//     }
+//      else if( arr[num] < 80)
+//     {
+//        console.log(`Student ${num} You are Failed`)
+//     }  
+// }
+
+
+// const myWork = [];
+
+
+// for (let i = 1; i <= 10; i++) {
+//   const status = i % 2 === 0 ? true : false; 
+//   const lesson = {
+//     name: `Lesson ${i}`,
+//     status: status
+//   };
+//   myWork.push(lesson);
+// }
+// console.log(myWork);
+
+
+
+
+
+// let arry = [];
+// console.log(arry)
+
+// for(let o=0; o<11; o++ )
+// {
+//     let find = o%2 === 0 ? true : false  
+//     // console.log(o)
+    
+//     let obbj =
+//     {
+//         name:`lesson ${o}`,
+//         Status: find  
+//     }
+//     arry.push(obbj)
+
+// }
+// console.log(arry)
+
+  
+
+
+// let arry = [];
+// console.log(arry)
+
+// for(let o=0; o<11; o++ )
+// {
+//     let find = o%2 
+//     // console.log(o)
+    
+//     let obbj =
+//     {
+//         name:`lesson ${o}`,
+//         Status: find === 0 ? true : false   
+//     }
+//     arry.push(obbj)
+
+// }
+// console.log(arry)
+ 
+// Is code mein, ek empty array arry banaya gaya hai, jise phir console par print kiya gaya hai. Phir ek for loop chalaya gaya hai, jis mein o variable ko 0 se lekar 10 tak increment kiya jata hai.
+
+// Har iteration mein, find variable mein o ka modulus 2 (o%2) store kiya jata hai. Modulus operation, ek number ko doosre number se divide karne par bachi hui remainder hasil karta hai. Is case mein, o ko 2 se divide karke, agar remainder 0 hota hai to find mein 0 aata hai, agar remainder 1 hota hai to find mein 1 aata hai.
+
+// Phir, ek object obbj banaya jata hai, jiska name property "lesson" ke sath o ki value se initialize ki gayi hai aur Status property mein find ki value se initialize ki gayi hai. Agar find ki value 0 hai (yaani o even number hai), to Status true hoga, agar find ki value 1 hai (yaani o odd number hai), to Status false hoga.
+
+// Is obbj object ko arry array mein push kiya jata hai, jis se har iteration mein ek naya object array mein add hota hai.
+
+// Loop khatam hone ke baad, arry array ko phir se console par print kiya jata hai.
+
+// Is code ki basic idea yeh hai ke ek array mein 0 se 10 tak ke lessons ki status (true ya false) ke sath objects create kiye jate hain. Jahan o even number hoga, wahan status true hoga, aur jahan o odd number hoga, wahan status false hoga.
+
+
+
+// Nested LooopS\
+
+// for(let i = 0 ; i<5; i++)
+// {
+//     console.log("I am I value",i)
+
+//     for(let b = 0 ; b<3; b++)
+//     console.log("I am B value",b)
+// }
+
+
+// while loop
+
+
+// let num = 0;
+// while(num < 10)
+// {
+//     console.log("Hello" , num)
+//     num++;
+// }
+
+
+
+
+// var min_value = 1;  
+// var max_value = 100;
+
+// var random_number = Math.floor(Math.random() * (max_value - min_value + 1));
+// console.log("Random Number:", random_number);
+// let user = (prompt("Enter yor Number"))
+// if(random_number == user )
+// {
+//     console.log("Right")
+
+// }
+
+// else
+// {
+//     console.log("Wrong")
+// }
+
+
+
+
+// let num = 0;
+// let somarry = ["name1","name2","name3","name4","name5","name6"]
+// while(num < 6)
+// {
+//     if(somarry[0] === "name4")
+//     {
+//         console.log("Found Her!!!")
+//         console.log(somarry)
+        
+//     }
+//     else
+//     {
+//         somarry.shift();
+//     }
+// num++;
+// }
+
+
+// var max_value = 50;
+
+// var random_number = Math.floor(Math.random() * (max_value  ,+ 1));
+// console.log("Random Number:", random_number);
+
+// let defult = true;
+
+// while(defult)
+// {
+//     user = (prompt("Enter Your Number", + max_value))
+//     user = Number (user);
+
+//      if(user === random_number)
+//      {
+//         defult = false;
+//         console.log("You GOt IT")
+//      }
+//      else if(user > random_number)
+//      {
+//         console.log("High")
+//      }
+//      else
+//      {
+//         console.log("Low")
+//      }
+
+// // }
+
+
+
+
+//********************************************* */ dooo while looop********************************
+
+
+
+
+// let number;
+
+// do {
+// number = prompt("Please enter a number between 0 and 100: ");
+// } while (!(number >= 0 && number < 100));
+// console.log(number)
+
+
+
+// let arry = []
+// for(let i = 0; i<10; i++)
+// {
+//   arry.push = "a";
+//   console.log(arry)
+//   for(let name of arry)
+//   {
+//     console.log(name)
+//   }
+
+// }
+
+
+
+// const myArray = [];
+
+// for (let i = 1; i <= 10; i++) {
+//   myArray.push(i);
+// }
+
+// console.log(myArray);
+
+// for (let j = 0; j < myArray.length; j++) {
+//   console.log(`Value at index ${j}: ${myArray[j]}`);
+// }
+// for (const value of myArray) {
+//   console.log(`Value: ${value}`);
+// }
+
+
+
+// let car = {
+//     model: "Golf",
+//     make: "Volkswagen",
+//     year: 1999,
+//     color: "black",
+//     };
+
+    
+//     for (let prop in car){
+//         console.log(prop);
+      
+//     console.log(car[prop]);
+//     }
+
+
+
+    // let obj =
+    // {
+    //     name : "abc",
+    //     class : 6,
+    //     subject : "Science"
+
+    // }
+
+    // for (let i in obj)
+    // {
+    //     console.log(i);
+    //     console.log(obj[i]);
+    // }
+
+    // let arry = []
+    
+    // arry.push = obj;
+    // console.log(arry)
+
+
+    // let car = {
+    //         model: "Golf",
+    //         make: "Volkswagen",
+    //         year: 1999,
+    //         color: "black",
+    //         };
+
+    //         let arrkey = Object.keys(car)
+    //         let arrv = Object.values(car)
+    //         console.log(arrkey)
+    //         console.log(arrv)
+
+    //         for( let i of arrkey)
+    //         {
+    //             console.log(i)
+    //         }
+    //         for( let h of arrv)
+    //         {
+    //             console.log(h)
+    //         }
+
+
+    // let car = {
+    //     model: "Golf",
+    //     make: "Volkswagen",
+    //     year: 1999,
+    //     color: "black",
+    //     };
+
+    //     let arry = Object.entries(car)
+    //     for(let [key , value] of arry)
+    //     {
+    //         console.log(key, ":", value);
+    //     }
+
+// for ( let i = 0; i < 10 ; i++)   
+// {
+//     console.log(i)
+//     if(i === 4)
+//     {
+//         break;
+//     }
+// }     
+
+let cars = [
+    {
+    model: "Golf",
+    make: "Volkswagen",
+    year: 1999,
+    color: "black",
+    },
+    {
+    model: "Picanto",
+    make: "Kia",
+    year: 2020,
+    color: "red",
+    },
+    {
+    model: "Peugeot",
+    make: "208",
+    year: 2021,
+    color: "black",
+    },
+    {
+    model: "Fiat",
+    make: "Punto",
+    year: 2020,
+    color: "black",
+    }
+    ];
+
+    for (let i = 0; i < cars.length; i++) {
+        if (cars[i].year >= 2020) {
+        if (cars[i].color === "black") {
+        console.log("I have found my new car:", cars[i]);
+        break;
+        }
+        }
+        }
